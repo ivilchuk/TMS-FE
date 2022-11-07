d7c60408b001884c376f2233c469d7e445b22e92
ivilchuk <i.vilchuk92@gmail.com>
2022-11-07 16:44:23 +0300

    lesson9

---
+++ lesson-9/css/script.js
@@ -0,0 +1,12 @@
+new Swiper(".image-slider", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullet: true,
+  },
+});
