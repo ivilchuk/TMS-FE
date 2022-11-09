ef5703eefa45877d63a3417c901b4bdb1a1804da
Ihar Vilchuk <i.vilchuk@MacBook-Air-Ihar.local>
2022-11-09 14:43:07 +0300

    lesson-9

--- lesson-9/css/script.js
+++ lesson-9/css/script.js
@@ -10,3 +10,16 @@ new Swiper(".image-slider", {
     dynamicBullet: true,
   },
 });
+
+
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
